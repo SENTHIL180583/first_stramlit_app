a72531751fee9a559d0c907c8636456a5b48b18d
SENTHIL180583 <157829904+SENTHIL180583@users.noreply.github.com>
2024-01-26 17:15:18 +0000

    menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Meanu')
+streamlit.text('Egg')
+streamlit.text('Bread')
+streamlit.text('French toast')
