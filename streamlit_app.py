d2f2373d665f6c837b6889be7ff45151dd3fae2f
SENTHIL180583 <157829904+SENTHIL180583@users.noreply.github.com>
2024-01-28 16:45:20 +0000

    new function

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,15 +20,22 @@ fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.in
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/kiwi")
+def get_fruityvice_data(this_fruit_choice):
+	frutyvice_responce=request.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+	return fruityvice_normalized 
+        
 streamlit.header("Fruityvice Fruit Advice!")
-#streamlit.text(fruityvice_response.json())
+try:
+   fruit_choice = streamlit.text_input('What fruit would you like information about?')
+   if not fruit_choice :
+	streamlit.error("please select a fruit to get information.")
+   else:
+	back_from_function(get_fruityvice_data(fruit_choice)
+	streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+    streamlit.error()
 
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
-
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
@@ -40,7 +47,3 @@ streamlit.dataframe(my_data_rows)
 
 #streamlit.write('Thanks for adding', add_my_fruit)
 my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('From Streamlit')")
-
-
-
-
