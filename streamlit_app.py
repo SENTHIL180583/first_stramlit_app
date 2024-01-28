4b35f370952fc3136ddaeab52efa04ba5c6951f5
SENTHIL180583 <157829904+SENTHIL180583@users.noreply.github.com>
2024-01-28 12:19:44 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,6 +36,9 @@ my_data_rows = my_cur.fetchall()
 streamlit.text("The fruit loaded contains:")
 streamlit.dataframe(my_data_rows)
 
+streamlit.write('Thanks for adding', add_my_fruit)
+my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('From Streamlit')"
+
 
 
 
